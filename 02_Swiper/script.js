e70fe36427d7628c80b5e752f88c1f951fac1b90
MateoDedi <mateod@live.com>
2023-10-11 14:17:20 +0200

    finished Swiper

--- 02_Swiper/script.js
+++ 02_Swiper/script.js
@@ -1,11 +1,13 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
-    loop: false,
+    loop: true,
+    slidesPerView: 3.5,
 
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
+        clickable: true,
     },
 
     // Navigation arrows
